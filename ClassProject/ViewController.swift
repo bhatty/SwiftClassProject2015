da8275a6dc6a97e80e6d116f69cf5fe1d9f5ada5
Fahim Bhatty <bhatty@gmail.com>
2015-12-19 12:25:41 -0600

    Initial Commit

---
+++ ClassProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClassProject
+//
+//  Created by Fahim Bhatty on 12/19/15.
+//  Copyright © 2015 Fahim Bhatty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
